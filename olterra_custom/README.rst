78f8458743d1f5d669285c9e2bf0ac3676ada423
Stéphan Sainléger <contact@stephansainleger.fr>
2023-11-06 16:05:44 +0100

    [NEW] olterra_custom: create add-on

---
+++ olterra_custom/README.rst
@@ -0,0 +1,43 @@
+===============
+olterra_custom
+===============
+
+Olterra Odoo customizations
+
+Installation
+============
+
+Use Odoo normal module installation procedure to install
+``olterra_custom``.
+
+Known issues / Roadmap
+======================
+
+None yet.
+Bug Tracker
+===========
+
+Bugs are tracked on `our issues website <https://github.com/elabore-coop/olterra-addons/issues>`_. In case of
+trouble, please check there if your issue has already been
+reported. If you spotted it first, help us smashing it by providing a
+detailed and welcomed feedback.
+
+Credits
+=======
+
+Contributors
+------------
+
+* Stéphan Sainléger
+
+Funders
+-------
+
+The development of this module has been financially supported by:
+* Elabore (https://elabore.coop)
+
+
+Maintainer
+----------
+
+This module is maintained by Elabore.
---
+++ olterra_custom/__manifest__.py
@@ -0,0 +1,38 @@
+# Copyright 2023 Stéphan Sainléger (Elabore)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "olterra_custom",
+    "version": "16.0.1.0.0",
+    "author": "Elabore",
+    "website": "https://elabore.coop",
+    "maintainer": "Stéphan Sainléger",
+    "license": "AGPL-3",
+    "category": "Tools",
+    "summary": "Olterra Odoo customizations",
+    # any module necessary for this one to work correctly
+    "depends": [
+        "base",
+    ],
+    "qweb": [
+        # "static/src/xml/*.xml",
+    ],
+    "external_dependencies": {
+        "python": [],
+    },
+    # always loaded
+    "data": [
+        # "security/security.xml",
+        # "views/menu.xml",
+        # "data/data.xml",
+    ],
+    # only loaded in demonstration mode
+    "demo": [],
+    "js": [],
+    "css": [],
+    "installable": True,
+    # Install this module automatically if all dependency have been previously
+    # and independently installed.  Used for synergetic or glue modules.
+    "auto_install": False,
+    "application": False,
+}
